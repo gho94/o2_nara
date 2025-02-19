7ad87ed9e5cdf12ee0e2ba451ac94bf592816c85
GeonHo Kim <yorjsgh@naver.com>
2025-02-19 13:52:25 +0900

    feat : HomeScreen 및 라우트 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,32 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:o2_nara/core/theme/app_theme.dart';
 import 'package:o2_nara/firebase_options.dart';
+import 'package:o2_nara/router.dart';
+import 'package:o2_nara/providers/theme_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MainApp());
+  runApp(const ProviderScope(child: MainApp()));
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends ConsumerWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isDarkMode = !ref.watch(themeProvider);
+
+    return MaterialApp.router(
+      title: 'O2Nara',
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: isDarkMode ? ThemeMode.dark : ThemeMode.light,
+      routerConfig: router,
     );
   }
 }
