13ab5d8fcf249f06e2a5c8239639930152b2e6a8
GeonHo Kim <yorjsgh@naver.com>
2025-02-19 13:53:16 +0900

    feat : 라우트용 샘플 페이지 추가

---
+++ lib/screens/auth/sign_up_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SignUpScreen extends StatelessWidget {
+  const SignUpScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('회원가입'),
+      ),
+      body: const Center(
+        child: Text('회원가입'),
+      ),
+    );
+  }
+}
