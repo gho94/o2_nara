40254c560f89501d4e058a00dd8e53548df6afec
GeonHo Kim <yorjsgh@naver.com>
2025-02-21 13:54:36 +0900

    feat : smallSpacing 추가

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -2,6 +2,7 @@ class AppConstants {
   static const double defaultPadding = 16.0;
   static const double defaultRadius = 8.0;
   static const double defaultSpacing = 16.0;
+  static const double smallSpacing = 8.0;
 
   static const Duration defaultAnimationDuration = Duration(milliseconds: 300);
   static const Duration shortAnimationDuration = Duration(milliseconds: 150);
